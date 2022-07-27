952788a5806c9bc413b0312ac338bc77be6d9e48
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2022-07-27 09:55:07 -0400

    beginning composition

---
+++ src/models/Sync.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosPromise } from 'axios';
+
+export class Sync<T> {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`)
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data.id;
+
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
