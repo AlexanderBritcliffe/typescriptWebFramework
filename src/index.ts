ce46aa476612a6f0e53457620cbb7abcd01848d2
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2022-08-01 19:37:17 -0400

    added attribute functions to User props type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: "new record" id: 1 });
 
-user.fetch();
+const on = user.on;
+
+user.on('change', () => {
+
+});
+
+//this retunrs on function from the events class
+//when we refrence user.on we are getting back a reference to on method on eventing class
--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,11 +1,27 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): (number | string) {
-    return this.data[propName]
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key]
   }
 
   set(update: T): void {
     Object.assign(this.data, update)
   }
 }
+
+//K is name age or ID T is type User Props
+//k extends key of t says we can only call get with name age or id
+//T[K] says look at the interface of T and return value at the key of k
+
+
+//k can only ever be namer age or id (we are using user props as T since only type in application thus far)
+
+//interface is the structure of an object
+
+//goal is for typescript to understand that whenn we call stirng name we will return a string
+
+//k extends keyof t is a generic constraint limits types that k can be
+//value of k can only be one of the keys of t
+
+//if T is user props k can only be name age or id
